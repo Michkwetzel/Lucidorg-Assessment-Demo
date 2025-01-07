75bf116032c8939c4e8ebb749e26e8fd3c8e094f
michaelwetzel <michkwetzel@gmail.com>
2025-01-07 11:37:36 +0200

    Now catches any errors and displays to user instead of crashing. Good UX

--- lib/main.dart
+++ lib/main.dart
@@ -2,65 +2,78 @@ import 'package:flutter/material.dart';
 import 'package:front_survey_questions/changeNotifiers/questionsProvider.dart';
 import 'package:front_survey_questions/changeNotifiers/radioButtonsState.dart';
 import 'package:front_survey_questions/changeNotifiers/ratingBarState.dart';
-import 'package:front_survey_questions/changeNotifiers/surveyInfoProvider.dart';
+import 'package:front_survey_questions/exceptions.dart';
+import 'package:front_survey_questions/screens/errorScreen.dart';
 import 'package:front_survey_questions/screens/finalScreen.dart';
 import 'package:front_survey_questions/screens/welcomeScreen.dart';
 import 'package:front_survey_questions/services/firestoreService.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:universal_html/js.dart';
 import 'firebase_options.dart';
 import 'package:logging/logging.dart';
 import 'package:universal_html/html.dart' as html;
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 Future<void> main() async {
-  void initializeLogging() {
-    Logger.root.level = Level.ALL;
-    Logger.root.onRecord.listen((record) {
-      print('${record.level.name}: ${record.time}: ${record.message}');
-    });
-  }
+  try {
+    void initializeLogging() {
+      Logger.root.level = Level.ALL;
+      Logger.root.onRecord.listen((record) {
+        print('${record.level.name}: ${record.time}: ${record.message}');
+      });
+    }
 
-  WidgetsFlutterBinding.ensureInitialized();
-  setUrlStrategy(PathUrlStrategy());
+    WidgetsFlutterBinding.ensureInitialized();
+    setUrlStrategy(PathUrlStrategy());
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
 
-  initializeLogging();
-  String? surveyToken;
-  String? companyUID;
+    initializeLogging();
+    String? surveyToken;
+    String? companyUID;
 
-  final uri = Uri.parse(html.window.location.href);
+    final uri = Uri.parse(html.window.location.href);
 
-  surveyToken = uri.queryParameters['token1'];
-  companyUID = uri.queryParameters['token2'];
-  surveyToken = 'NP9BdeiJ64AYHmTi9oXy';
-  companyUID = 'dBR3TMXWGxm_LqJDXd7vkw';
+    surveyToken = uri.queryParameters['token1'];
+    companyUID = uri.queryParameters['token2'];
 
-  runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (context) => RadioButtonState()),
-      ChangeNotifierProvider(create: (context) => Ratingbarstate()),
-      ChangeNotifierProvider(
-        create: (context) => QuestionsProvider(
-          radioButtonState: context.read<RadioButtonState>(),
-          ratingBarState: context.read<Ratingbarstate>(),
+    if (surveyToken == null || companyUID == null) {
+      throw MissingTokenException();
+    }
+
+    runApp(MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => RadioButtonState()),
+        ChangeNotifierProvider(create: (context) => Ratingbarstate()),
+        ChangeNotifierProvider(
+          create: (context) => QuestionsProvider(
+            radioButtonState: context.read<RadioButtonState>(),
+            ratingBarState: context.read<Ratingbarstate>(),
+          ),
         ),
-      ),
-      ChangeNotifierProvider(create: (context) => SurveyInfoProvider(alreadyAnswered: false)),
-      Provider<FirestoreService>(
-        create: (context) {
-          final questionsProvider = Provider.of<QuestionsProvider>(context, listen: false);
-          return FirestoreService(questions: questionsProvider, surveyToken: surveyToken, companyUID: companyUID);
-        },
-        lazy: true,
-      )
-    ],
-    child: MyApp(),
-  ));
+        Provider<FirestoreService>(
+          create: (context) {
+            final questionsProvider = Provider.of<QuestionsProvider>(context, listen: false);
+            return FirestoreService(questions: questionsProvider, surveyToken: surveyToken, companyUID: companyUID);
+          },
+          lazy: false,
+        )
+      ],
+      child: MyApp(),
+    ));
+  } on Exception catch (e) {
+    if (e is SurveyException) {
+      runApp(
+        ErrorScreen(e.message),
+      );
+    } else {
+      runApp(
+        ErrorScreen("Error, Please try again later or refresh browser"),
+      );
+    }
+  }
 }
 
 class MyApp extends StatelessWidget {
